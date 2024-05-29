3f1e080290dd17edd7f1db43bce68c1c0f97666f
stepit <stefanofrancesco.pitton@gmail.com>
2024-05-29 20:10:46 +0200

    add something

--- contracts/controller/src/state.rs
+++ contracts/controller/src/state.rs
@@ -1,28 +1,34 @@
 use abstract_adapter::objects::{chain_name::ChainName, AccountId};
 use cosmwasm_std::Addr;
-use cw_storage_plus::{ Item, Map, IndexedMap,};
+use cw_storage_plus::{IndexedMap, Item, Map};
 
+/// General configuration of the controller contract.
 #[cosmwasm_schema::cw_serde]
 pub struct Config {}
 
+/// Represent the position of a user in the board.
 pub type Position = u32;
-// default config
+
+/// Store the information of the contract configuration.
 pub const CONFIG: Item<Config> = Item::new("config");
+// TODO: whut?
 pub const STATUS: Map<&AccountId, String> = Map::new("status");
 
-// game controller state
+/// Store the information of the owner of the contracts.
 pub const OWNER: Item<&Addr> = Item::new("owner");
 
+/// Store the information of the position of every participant in the board.
 pub const PARTICIPANTS: Map<&Addr, Position> = Map::new("participants");
 
 /// A inclusive range of positions on the board
 #[cosmwasm_schema::cw_serde]
-pub struct PositionRange (Position, Position);
+pub struct PositionRange(Position, Position);
 
+/// The id of the board is represented by the name of the chain.
 pub type BoardId = ChainName;
 
+/// Stores the tiles associated to every chain in which a board is present.
 pub const BOARD_IDS: Map<&BoardId, PositionRange> = Map::new("board_ids");
-
 
 impl PositionRange {
     pub fn new(start: Position, end: Position) -> Self {
@@ -41,4 +47,3 @@ impl PositionRange {
         (self.0..=self.1).collect()
     }
 }
-
--- packages/common/src/board.rs
+++ packages/common/src/board.rs
@@ -1,4 +1,3 @@
-
 use abstract_adapter::{objects::AccountId, AdapterContract};
 use cosmwasm_schema::QueryResponses;
 use cosmwasm_std::{Addr, Coin};
@@ -17,7 +16,6 @@ pub struct RequiredAction {
     action_msgs: Vec<String>,
 }
 
-
 #[cosmwasm_schema::cw_serde]
 pub enum TileAction {
     Rugg { n_tile: u8 },
@@ -25,65 +23,65 @@ pub enum TileAction {
     Action { action: Option<RequiredAction> },
 }
 
-    /// Adapter instantiate message
-    #[cosmwasm_schema::cw_serde]
-    pub struct BoardInstantiateMsg {
-        /// The name of the chain in which this contract
-        /// is instantiated.
-        pub chain: String,
-        /// A vector containing the index of the tile and
-        /// the action to perform.
-        pub tiles_actions: Vec<(u32, TileAction)>,
-        /// Number of tiles associated with this chain.
-        pub tiles_number: u32,
-        /// Controller addres
-        pub controller_address: String,
-    }
+/// Adapter instantiate message
+#[cosmwasm_schema::cw_serde]
+pub struct BoardInstantiateMsg {
+    /// The name of the chain in which this contract
+    /// is instantiated.
+    pub chain: String,
+    /// A vector containing the index of the tile and
+    /// the action to perform.
+    pub tiles_actions: Vec<(u32, TileAction)>,
+    /// Number of tiles associated with this chain.
+    pub tiles_number: u32,
+    /// Controller addres
+    pub controller_address: String,
+}
 
-    /// Adapter execute messages
-    #[cosmwasm_schema::cw_serde]
-    #[derive(cw_orch::ExecuteFns)]
-    #[impl_into(ExecuteMsg)]
-    pub enum BoardExecuteMsg {
-        /// Set status of your account
-        SetStatus {
-            status: String,
-        },
-        /// Admin method: Update the configuration of the adapter
-        UpdateConfig {},
-        RegisterAction {
-            user: String,
-            tile_number: u32,
-        },
-        PerformAction {},
-    }
+/// Adapter execute messages
+#[cosmwasm_schema::cw_serde]
+#[derive(cw_orch::ExecuteFns)]
+#[impl_into(ExecuteMsg)]
+pub enum BoardExecuteMsg {
+    /// Set status of your account
+    SetStatus {
+        status: String,
+    },
+    /// Admin method: Update the configuration of the adapter
+    UpdateConfig {},
+    RegisterAction {
+        user: String,
+        tile_number: u32,
+    },
+    PerformAction {},
+}
 
-    /// Adapter query messages
-    #[cosmwasm_schema::cw_serde]
-    #[derive(QueryResponses, cw_orch::QueryFns)]
-    #[impl_into(QueryMsg)]
-    pub enum BoardQueryMsg {
-        #[returns(StatusResponse)]
-        Status { account_id: AccountId },
-        #[returns(ConfigResponse)]
-        Config {},
-        #[returns(OngoingActionResponse)]
-        OngoingAction {},
-    }
+/// Adapter query messages
+#[cosmwasm_schema::cw_serde]
+#[derive(QueryResponses, cw_orch::QueryFns)]
+#[impl_into(QueryMsg)]
+pub enum BoardQueryMsg {
+    #[returns(StatusResponse)]
+    Status { account_id: AccountId },
+    #[returns(ConfigResponse)]
+    Config {},
+    #[returns(OngoingActionResponse)]
+    OngoingAction {},
+}
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct ConfigResponse {}
+#[cosmwasm_schema::cw_serde]
+pub struct ConfigResponse {}
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct StatusResponse {
-        pub status: Option<String>,
-    }
+#[cosmwasm_schema::cw_serde]
+pub struct StatusResponse {
+    pub status: Option<String>,
+}
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct OngoingActionResponse {}
+#[cosmwasm_schema::cw_serde]
+pub struct OngoingActionResponse {}
 
-
-    #[cosmwasm_schema::cw_serde]
+#[cosmwasm_schema::cw_serde]
 pub enum BoardIbcMsg {
     RegisterAction { user: Addr, tile_number: u32 },
 }
+
